083a95c54c42d18eb05048cf1d40a8c7aa42c903
silvavini9 <silvavinicius2018@gmail.com>
2019-10-08 11:17:25 -0400

    Inicio De convalidação

--- src/controllers/PersonController.js
+++ src/controllers/PersonController.js
@@ -7,39 +7,40 @@ module.exports = {
         const { name, cpf }  = req.body;
 
         var firstUser = await Person.find({}, (user) => {
-            if(user > 0){
-                return false;
-            }
-            else {
-                return true;
-            }
+            console.log(user);
+        //     if(user > 0){
+        //         return false;
+        //     }
+        //     else {
+        //         return true;
+        //     }
         });
 
-        if(firstUser){
-            var person = new Person({
-                _id: ObjectId,
-                name: name,
-                CPF: cpf,
-                password: cpf,
-            });
-        }
-        else{
-            var person = new Person({
-                _id: ObjectId,
-                name: name,
-                CPF: cpf,
-                admin: true,
-                password: cpf,
-            });
-        } 
-        await person.save((error) => {
-            if(error){
-                res.json(error);
-            }
-            else{
-                res.json(person)
-            }
-        });
+        // if(firstUser){
+        //     var person = new Person({
+        //         _id: ObjectId,
+        //         name: name,
+        //         CPF: cpf,
+        //         password: cpf,
+        //     });
+        // }
+        // else{
+        //     var person = new Person({
+        //         _id: ObjectId,
+        //         name: name,
+        //         CPF: cpf,
+        //         admin: true,
+        //         password: cpf,
+        //     });
+        // } 
+        // await person.save((error) => {
+        //     if(error){
+        //         res.json(error);
+        //     }
+        //     else{
+        //         res.json(person)
+        //     }
+        // });
         
     }
 }
